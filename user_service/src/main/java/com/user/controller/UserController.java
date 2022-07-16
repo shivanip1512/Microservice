d6b9f7987c5ac66b562572052f2838e0254465a8
shivanip1512 <shivanipacharne10@gmail.com>
2022-07-17 02:49:21 +0530

    communication b/w microservices

--- user_service/src/main/java/com/user/controller/UserController.java
+++ user_service/src/main/java/com/user/controller/UserController.java
@@ -1,11 +1,20 @@
 package com.user.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.user.entity.Contact;
 import com.user.entity.User;
 import com.user.service.UserService;
 
@@ -13,10 +22,24 @@ import com.user.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 
-	@Autowired private UserService userService;
-	
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@SuppressWarnings("unchecked")
 	@GetMapping("/{userId}")
 	public User getUser(@PathVariable("userId") Long userId) {
-		return userService.getUser(userId);
+
+		ArrayList<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+		messageConverters.add(converter);
+		this.restTemplate.setMessageConverters(messageConverters);
+
+		User user = userService.getUser(userId);
+		List contacts = this.restTemplate.getForObject("http://localhost:9002/contact/user/" + userId, ArrayList.class);
+		user.setContacts((java.util.List<Contact>) contacts);
+		return user;
 	}
 }
